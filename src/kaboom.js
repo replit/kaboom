fab1c1dc67295f5213eb5f91cd6c84b9f71c403b
tga <tga@space55.xyz>
2021-04-03 14:43:41 -0400

    add license to code

--- src/kaboom.js
+++ src/kaboom.js
@@ -1,7 +1,24 @@
-// kaboom.js
-// v0.1.0
-
 /*
+
+kaboom.js
+v0.1.0
+
+a JavaScript game programming library
+
+Copyright (C) 2021 Replit
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 modules:
 
