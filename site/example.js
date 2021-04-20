cca11d706180370d73136ba459a8edbd5fa3b281
tga <tga@space55.xyz>
2021-04-19 23:25:53 -0400

    test twitter card

--- site/example.js
+++ site/example.js
@@ -6,12 +6,14 @@ module.exports = (name) => {
 
 	const page = t("html", {}, [
 		t("head", {}, [
-			t("meta", { name: "twitter:card", content: "summary_large_image", }),
+			t("meta", { name: "twitter:card", content: "player", }),
 			t("meta", { name: "twitter:site", content: "@Kaboomjs", }),
 			t("meta", { name: "twitter:title", content: name, }),
-			t("meta", { name: "twitter:description", content: name, }),
+			t("meta", { name: "twitter:description", content: `kaboom.js example - ${name}`, }),
 			t("meta", { name: "twitter:image", content: `https://kaboomjs.com/pub/img/chill.png`, }),
-			t("meta", { name: "twitter:url", content: `https://kaboomjs.com/example/${name}`, }),
+			t("meta", { name: "twitter:player", content: `https://kaboomjs.com/example/${name}`, }),
+			t("meta", { name: "twitter:player:width", content: "480", }),
+			t("meta", { name: "twitter:player:height", content: "480", }),
 			t("style", {}, www.style(gstyle)),
 			t("script", { src: "/lib/dev/kaboom.js", }, ""),
 		]),
