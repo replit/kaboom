44737bf6a7c11739e4600c4f718fa656e343c2b0
tga <tga@space55.xyz>
2021-08-25 00:17:25 -0400

    add comments to audio example

--- examples/audio.js
+++ examples/audio.js
@@ -12,6 +12,7 @@ loadSound("OtherworldlyFoe", "sounds/OtherworldlyFoe.mp3");
 // the music might not autoplay cuz some browser won't allow audio start before any user interaction
 const music = play("OtherworldlyFoe", { loop: true, });
 
+// adjust global volume
 volume(0.5);
 
 const label = add([
@@ -22,42 +23,32 @@ function updateText() {
 	label.text = `
 ${music.paused() ? "paused" : "playing"}
 time: ${music.time().toFixed(2)}
-volume: ${music.volume()}
-detune: ${music.detune()}
+volume: ${music.volume().toFixed(2)}
+detune: ${music.detune().toFixed(2)}
 	`.trim();
 }
 
 updateText();
+
+// update text every frame
 action(() => updateText());
 
 keyPress("space", () => {
 
+	// pause / play music
 	if (music.paused()) {
 		music.play();
 	} else {
 		music.pause();
 	}
 
+	// play one off sound
 	play("wooosh");
 
 });
 
-keyPress("down", () => {
-	music.volume(music.volume() - 0.1);
-});
-
-keyPress("up", () => {
-	music.volume(music.volume() + 0.1);
-});
-
-keyPress("left", () => {
-	music.detune(music.detune() - 100);
-});
-
-keyPress("right", () => {
-	music.detune(music.detune() + 100);
-});
-
-keyPress("escape", () => {
-	music.stop();
-});
+keyPress("up", () => music.volume(music.volume() + 0.1));
+keyPress("down", () => music.volume(music.volume() - 0.1));
+keyPress("left", () => music.detune(music.detune() - 100));
+keyPress("right", () => music.detune(music.detune() + 100));
+keyPress("escape", () => music.stop());
