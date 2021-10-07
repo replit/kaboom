c891a0a0c9b7425ac50a13d7812e00e3539c7239
tga <tga@space55.xyz>
2021-10-07 13:58:37 -0400

    move all legacy files to /public/legacy, and redirect

--- site/next.config.js
+++ site/next.config.js
@@ -3,10 +3,9 @@ module.exports = {
 	reactStrictMode: true,
 	async redirects(data) {
 		return [
-			{
-				source: "/demo",
-				destination: "/play",
-			},
+			{ source: "/demo", destination: "/play", permanent: false },
+			{ source: "/lib/:path*", destination: "/legacy/lib/:path*", permanent: false },
+			{ source: "/pub/legacy/kbmsprite.js", destination: "/legacy/kbmsprite.js", permanent: false },
 		]
 	},
 }
