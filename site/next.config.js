a633134f0e0b2e95e75dc8effd0c3ec7bddcefc5
tga <tga@space55.xyz>
2021-10-07 14:16:39 -0400

    redirect old routes to new routes (#262)
    * redirect /demo to /play
    
    * move all legacy files to /public/legacy, and redirect
    
    * cleanrer redir synta
    
    * revert redir syntax
    
    * use wildcard
    
    * syntax
    
    * route /doc to /

--- site/next.config.js
+++ site/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
 	reactStrictMode: true,
+	async redirects(data) {
+		return [
+			{ source: "/doc", destination: "/", permanent: false },
+			{ source: "/demo", destination: "/play", permanent: false },
+			{ source: "/lib/:path*", destination: "/legacy/lib/:path*", permanent: false },
+			{ source: "/pub/legacy/:path*", destination: "/legacy/:path*", permanent: false },
+		];
+	},
 }
