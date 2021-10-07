488c34076d97f6741d335929a57e83d2d0b32419
tga <tga@space55.xyz>
2021-10-07 14:11:53 -0400

    use wildcard

--- site/next.config.js
+++ site/next.config.js
@@ -5,7 +5,7 @@ module.exports = {
 		return [
 			{ source: "/demo", destination: "/play", permanent: false },
 			{ source: "/lib/:path*", destination: "/legacy/lib/:path*", permanent: false },
-			{ source: "/pub/legacy/kbmsprite.js", destination: "/legacy/kbmsprite.js", permanent: false },
+			{ source: "/pub/legacy/:path*", destination: "/legacy/:path*", permanent: false },
 		]
 	},
 }
