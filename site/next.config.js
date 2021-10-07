3143b50315d43febc1010a441b992e83dc25d327
tga <tga@space55.xyz>
2021-10-07 14:12:56 -0400

    syntax

--- site/next.config.js
+++ site/next.config.js
@@ -6,6 +6,6 @@ module.exports = {
 			{ source: "/demo", destination: "/play", permanent: false },
 			{ source: "/lib/:path*", destination: "/legacy/lib/:path*", permanent: false },
 			{ source: "/pub/legacy/:path*", destination: "/legacy/:path*", permanent: false },
-		]
+		];
 	},
 }
