0b85e9a8c089af9c94094133d49d4e8f6914c5f8
tga <tga@space55.xyz>
2021-10-18 17:32:58 -0400

    body should require area

--- src/kaboom.ts
+++ src/kaboom.ts
@@ -1926,7 +1926,7 @@ function body(conf: BodyCompConf = {}): BodyComp {
 	return {
 
 		id: "body",
-		require: [ "pos", ],
+		require: [ "pos", "body", ],
 		jumpForce: conf.jumpForce ?? DEF_JUMP_FORCE,
 		weight: conf.weight ?? 1,
 		solid: conf.solid ?? true,
