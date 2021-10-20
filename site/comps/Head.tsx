60e0c2e926036d59d64259ea24f5127ea690b49b
tga <tga@space55.xyz>
2021-10-20 19:57:59 -0400

    fix twitter card

--- site/comps/Head.tsx
+++ site/comps/Head.tsx
@@ -4,17 +4,20 @@ interface HeadProps {
 	title?: string,
 	desc?: string,
 	icon?: string,
+	img?: string,
 	scale?: number,
 }
 
 const DEF_TITLE = "Kaboom";
 const DEF_DESC = "JavaScript game programming library that helps you make games fast and fun";
 const DEF_ICON = "/site/img/k.png";
+const DEF_IMG = "https://kaboomjs.com/site/img/k.png";
 
 const Head: React.FC<HeadProps> = ({
 	title,
 	desc,
 	icon,
+	img,
 	scale,
 }) => (
 	<NextHead>
@@ -25,7 +28,8 @@ const Head: React.FC<HeadProps> = ({
 		<meta name="twitter:card" content="summary" />
 		<meta name="twitter:title" content={title ?? DEF_TITLE} />
 		<meta name="twitter:description" content={desc ?? DEF_DESC} />
-		<meta name="twitter:image" content={icon ?? DEF_ICON} />
+		<meta name="twitter:image" content={img ?? DEF_IMG} />
+		<meta name="twitter:site" content="@kaboomjs" />
 		<script defer data-domain="kaboomjs.com" src="https://plausible.io/js/plausible.js" />
 	</NextHead>
 );
