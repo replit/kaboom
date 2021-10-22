1e59c358ca11b3dc42ee9c561fc51e1cb25d14a2
tga <tga@space55.xyz>
2021-10-22 16:17:21 -0400

    layout structure

---
+++ demo/basics/audio.js
@@ -0,0 +1,50 @@
+// audio playback & control
+
+kaboom();
+
+loadSound("wooosh", "sounds/wooosh.mp3");
+loadSound("OtherworldlyFoe", "sounds/OtherworldlyFoe.mp3");
+
+// the music might not autoplay cuz some browser won't allow audio start before any user interaction
+const music = play("OtherworldlyFoe", { loop: true, });
+
+// adjust global volume
+volume(0.5);
+
+const label = add([
+	text(),
+]);
+
+function updateText() {
+	label.text = `
+${music.paused() ? "paused" : "playing"}
+time: ${music.time().toFixed(2)}
+volume: ${music.volume().toFixed(2)}
+detune: ${music.detune().toFixed(2)}
+	`.trim();
+}
+
+updateText();
+
+// update text every frame
+action(() => updateText());
+
+keyPress("space", () => {
+
+	// pause / play music
+	if (music.paused()) {
+		music.play();
+	} else {
+		music.pause();
+	}
+
+	// play one off sound
+	play("wooosh");
+
+});
+
+keyPress("up", () => music.volume(music.volume() + 0.1));
+keyPress("down", () => music.volume(music.volume() - 0.1));
+keyPress("left", () => music.detune(music.detune() - 100));
+keyPress("right", () => music.detune(music.detune() + 100));
+keyPress("escape", () => music.stop());
---
+++ demo/basics/text.js
@@ -0,0 +1,47 @@
+kaboom();
+
+const input = add([
+	text("123abc", {
+		width: width(),
+		size: 74,
+	}),
+]);
+
+// load custom font 8x8
+loadFont("unscii", "fonts/unscii_8x8.png", 8, 8);
+
+// list of built-in fonts ("o" at the end means the outlined version)
+const fonts = [
+	"apl386o",
+	"apl386",
+	"sinko",
+	"sink",
+	"unscii"
+];
+
+let curFont = 0;
+
+// listen to text input
+charInput((ch) => {
+	input.text += ch;
+});
+
+// like keyPress() but will retrigger when key is being held (which is similar
+// to text input behavior)
+keyPressRep("enter", () => {
+	input.text += "\n";
+});
+
+keyPressRep("backspace", () => {
+	input.text = input.text.substring(0, input.text.length - 1);
+});
+
+keyPress("up", () => {
+	curFont = Math.max(curFont - 1, 0);
+	input.font = fonts[curFont];
+});
+
+keyPress("down", () => {
+	curFont = Math.min(curFont + 1, fonts.length - 1);
+	input.font = fonts[curFont];
+});
