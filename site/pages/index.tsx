8d02d447374ba7e6eab8bb6a645eada49f0c29ce
tga <tga@space55.xyz>
2021-10-24 14:19:08 -0400

    don't show kaboom() doc again

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -108,7 +108,7 @@ Play with it yourself or check out the examples in the [Playground](/play)!
 		}) }
 
 		{ doc.typerefs.map((name) => {
-			if (name !== "KaboomCtx") {
+			if (name !== "KaboomCtx" && name !== "kaboom") {
 				return <Doc id={name} key={name} name={name} />
 			}
 		})}
