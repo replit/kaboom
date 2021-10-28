c06e54c63a1c079402ca366412027a3fbbe75d3f
tga <tga@space55.xyz>
2021-10-27 22:22:40 -0400

    fix redundant commit

--- src/types.ts
+++ src/types.ts
@@ -1949,6 +1949,8 @@ export interface GameObjRaw {
 	 */
 	onDestroy(cb: () => void): EventCanceller;
 	/**
+	 * Registers an event that runs every frame as long as the game obj exists (alias to onUpdate).
+	 *
 	 * @deprecated Use onUpdate() instead.
 	 */
 	action: GameObjRaw["onUpdate"];
