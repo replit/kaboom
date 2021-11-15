e5563ad8478adb1ad7643ac7e953baea28595dbb
tga <tga@space55.xyz>
2021-11-15 16:00:04 -0500

    add state demo

---
+++ demo/state.js
@@ -0,0 +1,98 @@
+// Use state to handle basic AI
+
+// Start kaboom
+kaboom()
+
+// Load assets
+loadSprite("bean", "/sprites/bean.png")
+loadSprite("ghosty", "/sprites/ghosty.png")
+
+const SPEED = 320
+const ENEMY_SPEED = 160
+const BULLET_SPEED = 960
+
+// Add player game object
+const player = add([
+	sprite("bean"),
+	pos(80, 40),
+	color(),
+	area(),
+	solid(),
+	origin("center"),
+])
+
+const enemy = add([
+	sprite("ghosty"),
+	pos(width() - 160, height() - 160),
+	origin("center"),
+	// This enemy cycle between 3 states, and start from "idle" state
+	state("idle", [ "idle", "attack", "move", ]),
+	"enemy",
+])
+
+// Run the callback once every time we enter "idle" state.
+// Here we stay "idle" for 0.5 second, then enter "attack" state.
+enemy.onStateEnter("idle", () => {
+	wait(0.5, () => enemy.enterState("attack"))
+})
+
+// When we enter "attack" state, we fire a bullet, and enter "move" state after 1 sec
+enemy.onStateEnter("attack", () => {
+
+	wait(1, () => enemy.enterState("move"))
+
+	// Don't do anything if player doesn't exist anymore
+	if (!player.exists()) return
+
+	const dir = player.pos.sub(enemy.pos).unit()
+
+	add([
+		pos(enemy.pos),
+		move(dir, BULLET_SPEED),
+		rect(12, 12),
+		area(),
+		cleanup(),
+		origin("center"),
+		"bullet",
+	])
+
+})
+
+enemy.onStateEnter("move", () => {
+	wait(2, () => enemy.enterState("idle"))
+})
+
+// Like .onUpdate() which runs every frame, but only runs when the current state is "move"
+// Here we move towards the player every frame if the current state is "move"
+enemy.onStateUpdate("move", () => {
+	if (!player.exists()) return
+	const dir = player.pos.sub(enemy.pos).unit()
+	enemy.move(dir.scale(ENEMY_SPEED))
+})
+
+// Have to manually call enterState() to trigger the onStateEnter("idle") event we defined above.
+enemy.enterState("idle")
+
+// Taking a bullet makes us disappear
+player.onCollide("bullet", (bullet) => {
+	destroy(bullet)
+	destroy(player)
+	addKaboom(bullet.pos)
+})
+
+// Register input handlers & movement
+onKeyDown("left", () => {
+	player.move(-SPEED, 0)
+})
+
+onKeyDown("right", () => {
+	player.move(SPEED, 0)
+})
+
+onKeyDown("up", () => {
+	player.move(0, -SPEED)
+})
+
+onKeyDown("down", () => {
+	player.move(0, SPEED)
+})
