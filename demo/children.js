372c7db161c286e3fb240c08250f37c3e33bccb7
Larry S <muppethead@mac.com>
2021-11-28 08:46:40 -0800

    Adjusted formatting

--- demo/children.js
+++ demo/children.js
@@ -4,24 +4,24 @@ loadSprite("bean", "/sprites/bean.png")
 loadSprite("ghosty", "/sprites/ghosty.png")
 
 const nucleus = add([
-	sprite("ghosty"),
-	pos(center()),
-	rotate(0),
-	origin("center"),
+  sprite("ghosty"),
+  pos(center()),
+  rotate(0),
+  origin("center"),
 ])
 
 // Add children
 for (let i = 12; i < 24; i++) {
 
-	nucleus.add([
-	  sprite("bean"),
-	  rotate(0),
-	  origin(vec2(i, i).scale(0.25)),
-		{
-		speed: i * 8,
-		},
-	])
-
+  nucleus.add([
+  sprite("bean"),
+  rotate(0),
+  origin(vec2(i, i).scale(0.25)),
+  {
+    speed: i * 8,
+  },
+  ])
+	
 }
 
 nucleus.onUpdate(() => {
