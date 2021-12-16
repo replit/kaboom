4bed5c5eb123b22960154d93a5d8d81ba12717fd
tga <tga@space55.xyz>
2021-12-16 18:28:49 -0500

    Disable automatic culling (#468)
    * disable culling
    
    * remove unused code

--- src/gfx.ts
+++ src/gfx.ts
@@ -9,7 +9,6 @@ import {
 	isVec3,
 	isColor,
 	isMat4,
-	testRectRect,
 } from "./math";
 
 import {
@@ -480,19 +479,10 @@ function gfxInit(gl: WebGLRenderingContext, gopt: GfxOpt): Gfx {
 			flush();
 		}
 
-		const objBound = { p1: vec2(Number.MAX_VALUE), p2: vec2(-Number.MAX_VALUE)};
-		const screenBound = { p1: vec2(-1), p2: vec2(1) };
-
 		for (const v of verts) {
 
-			// WebGL screen space coordinate [-1.0 ~ 1.0]
+			// normalized world space coordinate [-1.0 ~ 1.0]
 			const pt = toNDC(gfx.transform.multVec2(v.pos.xy()));
-
-			// get the bounding rectangle for the polygon
-			objBound.p1.x = Math.min(objBound.p1.x, pt.x);
-			objBound.p2.x = Math.max(objBound.p2.x, pt.x);
-			objBound.p1.y = Math.min(objBound.p1.y, pt.y);
-			objBound.p2.y = Math.max(objBound.p2.y, pt.y);
 
 			gfx.vqueue.push(
 				pt.x, pt.y, v.pos.z,
@@ -500,13 +490,6 @@ function gfxInit(gl: WebGLRenderingContext, gopt: GfxOpt): Gfx {
 				v.color.r / 255, v.color.g / 255, v.color.b / 255, v.opacity,
 			);
 
-		}
-
-		// if the object bound is totally outside, we don't render
-		if (!testRectRect(objBound, screenBound)) {
-			const num = verts.length * STRIDE;
-			gfx.vqueue.splice(gfx.vqueue.length - num, num);
-			return;
 		}
 
 		for (const i of indices) {
