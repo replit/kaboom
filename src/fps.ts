adf605e8b53079ac43d45f89dc09b89bf6b4ce2c
tga <tga@space55.xyz>
2022-02-01 12:20:37 -0500

    fix fps

--- src/fps.ts
+++ src/fps.ts
@@ -1,18 +1,18 @@
 export default class FPSCounter {
 
-	private buf: number[] = [];
+	private dts: number[] = [];
 	private timer: number = 0;
 	fps: number = 0;
 
 	tick(dt: number) {
 
-		this.buf.push(1 / dt);
+		this.dts.push(dt);
 		this.timer += dt;
 
 		if (this.timer >= 1) {
 			this.timer = 0;
-			this.fps = Math.round(this.buf.reduce((a, b) => a + b) / this.buf.length);
-			this.buf = [];
+			this.fps = Math.round(1 / (this.dts.reduce((a, b) => a + b) / this.dts.length));
+			this.dts = [];
 		}
 
 	}
