af437b1cdda16f08dda576e90b6e589a45177c0e
tga <tga@space55.xyz>
2022-03-09 14:07:53 -0500

    got basic kaboom working

--- test/test.ts
+++ test/test.ts
@@ -1,12 +1,19 @@
 import puppeteer from "puppeteer"
+import kaboom from "kaboom"
 
 (async () => {
 	console.log("launching")
 	const browser = await puppeteer.launch();
 	console.log("creating new page")
 	const page = await browser.newPage();
-	console.log("going to example.com")
-	await page.goto("https://example.com");
+	await page.addScriptTag({ path: "../dist/kaboom.js" });
+	await page.evaluate(async () => {
+		kaboom()
+		loadBean()
+		add([
+			sprite("bean"),
+		])
+	})
 	console.log("taking screenshot")
 	await page.screenshot({ path: "example.png" });
 	console.log("done")
