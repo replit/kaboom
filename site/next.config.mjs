61e92ef5d743aaffd92658c213b2378cfe5146f4
Danie Báez <lajbelms@gmail.com>
2022-06-26 14:36:41 -0300

    Added a redirect to Discord Server
    To improve on the issue of authenticity when providing a Discord Server link

--- site/next.config.mjs
+++ site/next.config.mjs
@@ -13,6 +13,7 @@ export default {
 			{ source: "/doc", destination: "/", permanent: false },
 			{ source: "/lib/:path*", destination: "/legacy/lib/:path*", permanent: false },
 			{ source: "/pub/legacy/:path*", destination: "/legacy/:path*", permanent: false },
+			{ source: "/discord", destination: "https://discord.com/invite/aQ6RuQm3TF", permanent: false },
 		];
 	},
 }
