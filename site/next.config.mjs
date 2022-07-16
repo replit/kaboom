c0c37b1ed81b08da1df5e7b3743d59e9b6d468b7
tga <tga@space55.xyz>
2022-07-16 04:15:03 -0400

    remove legacy routes

--- site/next.config.mjs
+++ site/next.config.mjs
@@ -7,9 +7,6 @@ export default {
 		] },
 	],
 	redirects: async () => [
-		{ source: "/doc", destination: "/", permanent: false },
-		{ source: "/lib/:path*", destination: "/legacy/lib/:path*", permanent: false },
-		{ source: "/pub/legacy/:path*", destination: "/legacy/:path*", permanent: false },
 		{ source: "/discord", destination: "https://discord.com/invite/aQ6RuQm3TF", permanent: false },
 	],
 }
