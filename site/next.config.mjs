1d77aa4ef41b3d0846940bf752d47bcb53ad1b3e
tga <tga@space55.xyz>
2022-07-16 04:14:28 -0400

    upgrade to nextjs 12.2

--- site/next.config.mjs
+++ site/next.config.mjs
@@ -1,19 +1,15 @@
 export default {
 	reactStrictMode: true,
 	swcMinify: true,
-	async headers() {
-		return [
-			{ source: "/:path*", headers: [
-				{ key: "Access-Control-Allow-Origin", value: "*" },
-			] },
-		]
-	},
-	async redirects(data) {
-		return [
-			{ source: "/doc", destination: "/", permanent: false },
-			{ source: "/lib/:path*", destination: "/legacy/lib/:path*", permanent: false },
-			{ source: "/pub/legacy/:path*", destination: "/legacy/:path*", permanent: false },
-			{ source: "/discord", destination: "https://discord.com/invite/aQ6RuQm3TF", permanent: false },
-		]
-	},
+	headers: async () => [
+		{ source: "/:path*", headers: [
+			{ key: "Access-Control-Allow-Origin", value: "*" },
+		] },
+	],
+	redirects: async () => [
+		{ source: "/doc", destination: "/", permanent: false },
+		{ source: "/lib/:path*", destination: "/legacy/lib/:path*", permanent: false },
+		{ source: "/pub/legacy/:path*", destination: "/legacy/:path*", permanent: false },
+		{ source: "/discord", destination: "https://discord.com/invite/aQ6RuQm3TF", permanent: false },
+	],
 }
