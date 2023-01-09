69b28bedd544706edcaba9f72b198127c8ac3d0b
Marc Flerackers <mflerackers@gmail.com>
2023-01-09 10:01:13 +0900

    Correct terminology

--- src/math.ts
+++ src/math.ts
@@ -720,15 +720,15 @@ export function testLineCircle(l: Line, circle: Circle): boolean {
 	const centerToOrigin = l.p1.sub(circle.center)
 	const b = 2 * v.dot(centerToOrigin)
 	const c = centerToOrigin.dot(centerToOrigin) - circle.radius * circle.radius
-	// Calculate the determinant of ax^2 + bx + c
-	const det = b * b - 4 * a * c
+	// Calculate the discriminant of ax^2 + bx + c
+	const dis = b * b - 4 * a * c
 
 	// No root
-	if ((a <= Number.EPSILON) || (det < 0)) {
+	if ((a <= Number.EPSILON) || (dis < 0)) {
 		return false
 	}
 	// One possible root
-	else if (det == 0) {
+	else if (dis == 0) {
 		const t = -b / (2 * a)
 		if (t < 0 || t > 1) {
 			return false
@@ -736,8 +736,8 @@ export function testLineCircle(l: Line, circle: Circle): boolean {
 	}
 	// Two possible roots
 	else {
-		const t1 = (-b + Math.sqrt(det)) / (2 * a)
-		const t2 = (-b - Math.sqrt(det)) / (2 * a)
+		const t1 = (-b + Math.sqrt(dis)) / (2 * a)
+		const t2 = (-b - Math.sqrt(dis)) / (2 * a)
 		if ((t1 < 0 || t1 > 1) && (t2 < 0 || t2 > 1)) {
 			return false
 		}
