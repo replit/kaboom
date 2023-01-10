51044bfa56d907666192e5932d8f4871925eabff
tga <tga@space55.xyz>
2023-01-11 00:09:27 +0800

    fix blockly theme

--- site/comps/Blockly.tsx
+++ site/comps/Blockly.tsx
@@ -4,26 +4,15 @@ import {
 	useImperativeHandle,
 	forwardRef,
 } from "react"
-import Blockly, { WorkspaceSvg } from "blockly"
+import Blockly, { WorkspaceSvg, BlockSvg } from "blockly"
 import { javascriptGenerator } from "blockly/javascript"
 
 export interface BlocklyEditorRef {
 	genCode: () => string,
 }
 
-Blockly.registry.unregister("theme", "kaboom")
-
 const FONT_SIZE = 16
-const ICON_SIZE = FONT_SIZE * 1.75
-
-const theme = Blockly.Theme.defineTheme("kaboom", {
-	base: Blockly.Themes.Classic,
-	fontStyle: {
-		"family": "IBM Plex Mono",
-		"weight": "bold",
-		"size": FONT_SIZE,
-	},
-})
+const ICON_SIZE = FONT_SIZE * 1.6
 
 Blockly.defineBlocksWithJsonArray([{
 	"type": "kaboom",
@@ -82,6 +71,34 @@ Blockly.defineBlocksWithJsonArray([{
 	"colour": 200,
 	"tooltip": "Component to render a sprite",
 	"helpUrl": "https://kaboomjs.com#sprite",
+}])
+
+Blockly.defineBlocksWithJsonArray([{
+	"type": "pos",
+	"message0": "%1 pos %2 %3",
+	"args0": [
+		{
+			"type": "field_image",
+			"src": "https://github.com/replit/kaboom/raw/master/sprites/bean.png",
+			"width": ICON_SIZE,
+			"height": ICON_SIZE,
+			"alt": "*",
+		},
+		{
+			"type": "field_number",
+			"name": "x",
+			"value": 0,
+		},
+		{
+			"type": "field_number",
+			"name": "y",
+			"value": 0,
+		},
+	],
+	"output": "Object",
+	"colour": 200,
+	"tooltip": "Component to set object position",
+	"helpUrl": "https://kaboomjs.com#pos",
 }])
 
 Blockly.defineBlocksWithJsonArray([{
@@ -163,25 +180,129 @@ Blockly.defineBlocksWithJsonArray([{
 	"helpUrl": "https://kaboomjs.com#rotate",
 }])
 
-javascriptGenerator["kaboom"] = (block) => {
-	return ["kaboom()", javascriptGenerator.ORDER_FUNCTION_CALL]
+javascriptGenerator["kaboom"] = (block: BlockSvg) => {
+	console.log(block)
+	return "kaboom()"
 }
 
-javascriptGenerator["add"] = (block) => {
+javascriptGenerator["add"] = (block: BlockSvg) => {
 	return ["add()", javascriptGenerator.ORDER_FUNCTION_CALL]
 }
 
-javascriptGenerator["sprite"] = (block) => {
+javascriptGenerator["sprite"] = (block: BlockSvg) => {
 	return ["sprite()", javascriptGenerator.ORDER_FUNCTION_CALL]
 }
 
-javascriptGenerator["anchor"] = (block) => {
+javascriptGenerator["pos"] = (block: BlockSvg) => {
+	return ["pos()", javascriptGenerator.ORDER_FUNCTION_CALL]
+}
+
+javascriptGenerator["color"] = (block: BlockSvg) => {
+	return ["color()", javascriptGenerator.ORDER_FUNCTION_CALL]
+}
+
+javascriptGenerator["anchor"] = (block: BlockSvg) => {
 	return ["anchor()", javascriptGenerator.ORDER_FUNCTION_CALL]
 }
 
-javascriptGenerator["rotate"] = (block) => {
+javascriptGenerator["rotate"] = (block: BlockSvg) => {
 	return ["rotate()", javascriptGenerator.ORDER_FUNCTION_CALL]
 }
+
+const blocks = [
+	{
+		name: "kaboom",
+		blocks: [
+			"kaboom",
+			"sprite",
+			"pos",
+			"color",
+			"add",
+			"anchor",
+			"rotate",
+		],
+	},
+	{
+		name: "logic",
+		blocks: [
+			"controls_if",
+			"logic_boolean",
+			"logic_compare",
+			"logic_operation",
+			"logic_negate",
+			"logic_null",
+			"logic_ternary",
+		],
+	},
+	{
+		name: "controls",
+		blocks: [
+			"controls_repeat",
+			"controls_repeat_ext",
+		],
+	},
+	{
+		name: "math",
+		blocks: [
+			"math_number",
+			"math_arithmetic",
+			"math_trig",
+			"math_constant",
+			"math_number_property",
+			"math_round",
+			"math_on_list",
+			"math_modulo",
+			"math_constrain",
+			"math_random_int",
+			"math_random_float",
+			"math_atan2",
+		],
+	},
+	{
+		name: "text",
+		blocks: [
+			"text",
+			"text_multiline",
+			"text_create_join_container",
+			"text_length",
+			"text_isEmpty",
+			"text_indexOf",
+			"text_charAt",
+			"text_append",
+			"text_print",
+		],
+	},
+	{
+		name: "variables",
+		blocks: [
+			"variables_get",
+			"variables_set",
+			"math_change",
+		],
+	},
+	{
+		name: "list",
+		blocks: [
+			"lists_create_with",
+			"lists_create_empty",
+			"lists_repeat",
+			"lists_reverse",
+			"lists_isEmpty",
+			"lists_length",
+			"lists_getIndex",
+			"lists_setIndex",
+		],
+	},
+	{
+		name: "function",
+		blocks: [
+			"procedures_defnoreturn",
+			"procedures_defreturn",
+			"procedures_callnoreturn",
+			"procedures_callreturn",
+		],
+	},
+]
 
 const BlocklyEditor = forwardRef(({...props}, ref) => {
 	const divRef = useRef(null)
@@ -194,38 +315,46 @@ const BlocklyEditor = forwardRef(({...props}, ref) => {
 	}))
 	useEffect(() => {
 		if (!divRef.current) return
-		const blocks = [
-			"logic_boolean",
-			"logic_compare",
-			"logic_operation",
-			"logic_negate",
-			"logic_null",
-			"logic_ternary",
-			"controls_if",
-			"controls_repeat_ext",
-			"math_number",
-			"math_arithmetic",
-			"text",
-			"text_print",
-			"variables_get",
-			"variables_set",
-			"kaboom",
-			"sprite",
-			"color",
-			"add",
-			"anchor",
-			"rotate",
-		]
+		Blockly.registry.unregister("theme", "kaboom")
 		const workspace = Blockly.inject(divRef.current, {
 			toolbox: {
-				kind: "flyoutToolbox",
-				contents: blocks.map((b) => ({
-					"kind": "block",
-					"type": b,
+				kind: "categoryToolbox",
+				contents: blocks.map((c) => ({
+					kind: "category",
+					name: c.name,
+					contents: c.blocks.map((b) => ({
+						kind: "block",
+						type: b,
+					})),
 				})),
 			},
+			grid: {
+				spacing: 16,
+			},
+			zoom: {
+				controls: true,
+				pinch: true,
+				wheel: true,
+			},
 			trashcan: true,
-			theme: theme,
+			theme: {
+				name: "kaboom",
+				base: Blockly.Themes.Classic,
+				fontStyle: {
+					family: "IBM Plex Mono",
+					weight: "bold",
+					size: FONT_SIZE,
+				},
+				componentStyles: {
+					toolboxBackgroundColour: "#eeeeee",
+					toolboxForegroundColour: "#888888",
+					flyoutBackgroundColour: "#eeeeee",
+					flyoutForegroundColour: "#888888",
+					flyoutOpacity: 0.5,
+					insertionMarkerColour: "#fff",
+					scrollbarOpacity: 0.5,
+				},
+			},
 		})
 		workspaceRef.current = workspace
 		return () => workspace.dispose()
