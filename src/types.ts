d8df354f3d2a6f813faf349bc4a50470575ff87d
lajbel <lajbelms@gmail.com>
2023-02-03 13:55:55 -0300

    fmt and update addLevel example

--- src/types.ts
+++ src/types.ts
@@ -1878,25 +1878,27 @@ export interface KaboomCtx {
 	 *     "       ^^      = >    =   &",
 	 *     "===========================",
 	 * ], {
-	 *     // define the size of each block
-	 *     width: 32,
-	 *     height: 32,
+	 *     // define the size of tile block
+	 *     tileWidth: 32,
+	 *     tileHeight: 32,
 	 *     // define what each symbol means, by a function returning a component list (what will be passed to add())
-	 *     "=": () => [
-	 *         sprite("floor"),
-	 *         area(),
-	 *         solid(),
-	 *     ],
-	 *     "$": () => [
-	 *         sprite("coin"),
-	 *         area(),
-	 *         pos(0, -9),
-	 *     ],
-	 *     "^": () => [
-	 *         sprite("spike"),
-	 *         area(),
-	 *         "danger",
-	 *     ],
+	 *     tiles: {
+	 *         "=": () => [
+	 *             sprite("floor"),
+	 *             area(),
+	 *             solid(),
+	 *         ],
+	 *         "$": () => [
+	 *             sprite("coin"),
+	 *             area(),
+	 *             pos(0, -9),
+	 *         ],
+	 *         "^": () => [
+	 *             sprite("spike"),
+	 *             area(),
+	 *             "danger",
+	 *         ],
+	 *     }
 	 * })
 	 * ```
 	 */
