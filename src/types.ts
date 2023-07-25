2b0d8527b617c2a82ebe53dc9d0cb2a3ccd98c0c
Nami W <1160688+namiwang@users.noreply.github.com>
2023-07-25 18:54:35 +0000

    doc: fix OffScreenCompOpt.distance default value (#738)
    The value is actually 200 in [source](https://github.com/replit/kaboom/blob/aa44a62999b6b510c24e317fc76ffc64524e9680/src/kaboom.ts#L3743C1-L3744C1)

--- src/types.ts
+++ src/types.ts
@@ -3983,7 +3983,7 @@ export interface OffScreenCompOpt {
 	 */
 	destroy?: boolean,
 	/**
-	 * The distance when out of view is triggered (default 64).
+	 * The distance when out of view is triggered (default 200).
 	 *
 	 * @since v3000.0
 	 */
