90f7bcdcb2280f9bfde5bac4012f0fbedc79cfdf
luluxia <306924625@qq.com>
2023-10-16 14:54:10 +0800

    Fix emoji text color

--- src/kaboom.ts
+++ src/kaboom.ts
@@ -2111,7 +2111,7 @@ export default (gopt: KaboomOpt = {}): KaboomCtx => {
 		if (tr.pos) fchar.pos = fchar.pos.add(tr.pos)
 		if (tr.scale) fchar.scale = fchar.scale.scale(vec2(tr.scale))
 		if (tr.angle) fchar.angle += tr.angle
-		if (tr.color) fchar.color = fchar.color.mult(tr.color)
+		if (tr.color) fchar.color = runes(fchar.ch).length !== fchar.ch.length ? Color.WHITE : fchar.color.mult(tr.color)
 		if (tr.opacity) fchar.opacity *= tr.opacity
 	}
 
