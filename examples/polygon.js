8be28f3170d37a22059b333ba5fed9a8204e9861
Trevor Dilley <trevor.lee.dilley@gmail.com>
2023-10-21 16:58:41 -0700

    Removes extraneous comment, reorders components to polygon() is first in example

--- examples/polygon.js
+++ examples/polygon.js
@@ -1,9 +1,8 @@
-// Start the game in burp mode
 kaboom()
 
 add([
-	pos(80, 120),
 	polygon([vec2(0,0), vec2(50,0), vec2(50,50), vec2(0,50)]),
+	pos(80, 120),
 	outline(4),
 	area(),
 	color(rgb(255,0,0)),
