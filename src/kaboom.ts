a2126dd83f9cff3d8990ad3b8dc8e0c9578d6bad
neverUsedGithub <81071335+neverUsedGithub@users.noreply.github.com>
2024-03-10 20:08:50 +0100

    fix text styles breaking on newlines/wraps

--- src/kaboom.ts
+++ src/kaboom.ts
@@ -2522,14 +2522,14 @@ export default (gopt: KaboomOpt = {}): KaboomCtx => {
 
 		const fchars: FormattedChar[] = []
 
-		for (const line of lines) {
+		for (let i = 0; i < lines.length; i++) {
 
-			const ox = (tw - line.width) * alignPt(opt.align ?? "left")
+			const ox = (tw - lines[i].width) * alignPt(opt.align ?? "left")
 
-			for (const fchar of line.chars) {
+			for (const fchar of lines[i].chars) {
 
 				const q = font.map[fchar.ch]
-				const idx = fchars.length
+				const idx = fchars.length + i
 
 				fchar.pos = fchar.pos.add(ox, 0).add(
 					q.w * scale.x * 0.5,
