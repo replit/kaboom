50af28744bc4a44e8bc4528789f16d49d76bb2df
Eli <78963198+niceEli@users.noreply.github.com>
2024-04-07 12:45:48 -0400

    fix issues from comments

--- examples/fadeIn.js
+++ examples/fadeIn.js
@@ -1,61 +1,19 @@
-// Adding game objects to screen
-
-// Start a kaboom game
 kaboom()
+loadBean()
 
-// Load a sprite asset from "sprites/bean.png", with the name "bean"
-loadSprite("bean", "/sprites/bean.png")
-loadSprite("ghosty", "/sprites/ghosty.png")
-
-// A "Game Object" is the basic unit of entity in kaboom
-// Game objects are composed from components
-// Each component gives a game object certain capabilities
-
-// add() assembles a game object from a list of components and add to game, returns the reference of the game object. this object will fadeIn to opacty 1
-const player = add([
-	sprite("bean"),   // sprite() component makes it render as a sprite
-	pos(120, 80),     // pos() component gives it position, also enables movement
-	rotate(0),        // rotate() component gives it rotation
-	anchor("center"), // anchor() component defines the pivot point (defaults to "topleft")
-	opacity(),        // opacity() component gives it opacity
+// spawn a bean that takes a second to fade in
+const bean = add([
+	sprite("bean"),
+	pos(120, 80), 
+	opacity(),        // opacity() component gives it opacity which is required for fadeIn
 	fadeIn(),         // fadeIn() component makes it fade in
 ])
 
-// add() assembles a game object from a list of components and add to game, returns the reference of the game object. this object will fadeIn to opacty 0.5
-let invisibleBean = add([
-	sprite("bean"),   // sprite() component makes it render as a sprite
-	pos(240, 80),     // pos() component gives it position, also enables movement
-	rotate(0),        // rotate() component gives it rotation
-	anchor("center"), // anchor() component defines the pivot point (defaults to "topleft")
-	opacity(0.5),     // opacity() component gives it opacity (set to 0.5 so it will be half transparent)
-	fadeIn(),         // fadeIn() component makes it fade in
+// spawn another bean that takes 5 seconds to fade in halfway
+// SPOOKY!
+let spookyBean = add([
+	sprite("bean"),
+	pos(240, 80),
+	opacity(0.5),     // opacity() component gives it opacity which is required for fadeIn (set to 0.5 so it will be half transparent)
+	fadeIn(5),        // fadeIn() component makes it fade in (set to 5 so that it takes 5 seconds to fade in)
 ])
-
-// .onUpdate() is a method on all game objects, it registers an event that runs every frame
-player.onUpdate(() => {
-	// .angle is a property provided by rotate() component, here we're incrementing the angle by 120 degrees per second, dt() is the time elapsed since last frame in seconds
-	player.angle += 120 * dt()
-})
-
-// .onUpdate() is a method on all game objects, it registers an event that runs every frame
-invisibleBean.onUpdate(() => {
-	// .angle is a property provided by rotate() component, here we're incrementing the angle by 120 degrees per second, dt() is the time elapsed since last frame in seconds
-	invisibleBean.angle += -120 * dt()
-})
-
-// Add multiple game objects
-for (let i = 0; i < 3; i++) {
-
-	// generate a random point on screen
-	// width() and height() gives the game dimension
-	const x = rand(0, width())
-	const y = rand(0, height())
-
-	add([
-		sprite("ghosty"),
-		pos(x, y),
-		opacity(0.25),     // opacity() component gives it opacity (set to 0.25 so it will be quarter transparent)
-		fadeIn(),         // fadeIn() component makes it fade in
-	])
-
-}
